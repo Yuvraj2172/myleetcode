ff17ef36cfdd23be637db939b6b0cbda3cc6041e
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-01-16 17:33:19 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (52.98%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto ch : s){
+            if(ch=='(' or ch=='[' or ch=='{')st.push(ch);
+            else {
+                if(st.empty() or (st.top()=='(' and ch!=')')  or (st.top()=='[' and ch!=']') or (st.top()=='{' and ch!='}'))return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
