183f3d5e94c4c875092bfbd9851499d9444d5ac7
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-01-23 22:44:34 +0530

    Time: 24 ms (23.26%), Space: 20.3 MB (68.94%) - LeetHub

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -0,0 +1,20 @@
+
+class Solution {
+public:
+    // Solution(){
+    //     ios_base::sync_with_stdio(false);
+    //     cin.tie(NULL);
+    // }
+    int height(TreeNode* root){
+    if(root == NULL)return 0;
+    return 1 + max(height(root->left) , height(root->right));
+        }
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(root == NULL)return 0;
+        int option1 = height(root->left) + height(root->right);
+        int option2 = diameterOfBinaryTree(root->left);
+        int option3 = diameterOfBinaryTree(root->right);
+        int temp = max(option1 , option2);
+        return max(temp , option3);
+    }
+};
