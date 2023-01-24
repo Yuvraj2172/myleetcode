a5aac42bd965f8130ed1d1e4139c2d55ee66a267
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-01-25 00:55:25 +0530

    Time: 15 ms (66.91%), Space: 21 MB (59.87%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -0,0 +1,20 @@
+
+class Solution {
+public:
+    Solution(){
+    ios::sync_with_stdio(false);
+        cin.tie(NULL);
+    }
+    int height(TreeNode* root){
+        if(root == NULL)return 0;
+       int lh = height(root->left);
+        if(lh == -1)return -1;
+        int rh = height(root->right);
+        if(rh == -1)return -1;
+        if( abs(lh - rh )> 1)return -1;
+        return 1 + max(lh, rh);
+    }
+    bool isBalanced(TreeNode* root) {
+      return height(root)!=-1;
+    }
+};
