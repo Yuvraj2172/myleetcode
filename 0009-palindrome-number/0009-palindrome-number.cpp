600613b57cad3bb2d586de20622997983759c2b0
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-02-14 09:12:03 +0530

    Time: 20 ms (43.03%), Space: 6 MB (62.37%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string a = to_string(x);
+        string b = a;
+        reverse(b.begin() , b.end());
+        return a==b;
+    }
+};
