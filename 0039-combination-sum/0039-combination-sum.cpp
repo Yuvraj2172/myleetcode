cefda7dfcb98e3ec07d85c5364bd924b6c41ac0f
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-02-16 21:55:05 +0530

    Time: 71 ms (18.72%), Space: 48 MB (16.03%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void helper(vector<int> arr, int target , vector<vector<int>>& ans , vector<int>& temp , int ind){
+        if(ind == arr.size()){
+            if(target==0){
+                ans.push_back(temp);
+            }
+             return;
+        }
+        if(arr[ind] <= target){
+            temp.push_back(arr[ind]);
+            helper(arr , target - arr[ind], ans , temp , ind);
+            temp.pop_back();
+        }
+        helper(arr, target , ans , temp, ind+1);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> temp;
+        vector<vector<int>> ans;
+        helper(candidates, target , ans , temp, 0);
+        return ans;
+    }
+};
