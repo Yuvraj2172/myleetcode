e86464738a2e7a1d832ff39165470e540ba4e98e
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-02-19 23:13:48 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (62.82%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long long int left=0,right=100000;
-        long long int mid=(left+right)/2;
-        while(true)
-        {
-            long long int temp=mid;
-            if (mid*mid>x) right=mid+1;else left=mid;
-            mid=(left+right)/2;
-            if (temp==mid) break;
-        }
-            
-        return left;
+        return pow(x,0.5);
     }
 };
