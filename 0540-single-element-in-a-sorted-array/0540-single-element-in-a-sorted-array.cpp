21fd50e98077ed1012897fac537a97e1c725a4b4
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-02-21 10:17:59 +0530

    Time: 33 ms (37.69%), Space: 22.3 MB (62.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-     map<int , int > mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
-        }
-        for(auto it : mp){
-            if(it.second == 1)return it.first;
-        }
-        return 0;
+     int x = 0;
+        for(int i=0;i<nums.size();i++)x = x^ nums[i];
+        return x;
     }
 };
