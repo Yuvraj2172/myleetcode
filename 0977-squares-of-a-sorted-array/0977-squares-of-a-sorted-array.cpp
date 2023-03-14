4f2a78f6a0edec63df031d52d62aa982fe85a436
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-03-14 09:45:22 +0530

    Time: 37 ms (65.87%), Space: 26.8 MB (31.44%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -11,24 +11,25 @@ public:
         int j = i-1;
         cout<<j<<" "<<i;
         vector<int> ans;
-        while(i<=nums.size()-1 && j>=0){
-            if(nums[i]*nums[i] <= nums[j]*nums[j]){
-                ans.push_back(nums[i]*nums[i]);
-                i++;
-            }
-            else{
-                ans.push_back(nums[j]*nums[j]);
-                j--;
-            }
-        }
-        while(i<=nums.size()-1){
-            ans.push_back(nums[i]*nums[i]);
+        while(i<=nums.size()-1 || j>=0){
+            int val1=1e9,val2=1e9;
+            if(i<nums.size())
+                val1 = nums[i]*nums[i];
+            if(j>=0)
+                val2 = nums[j]*nums[j];
+            if(val1<val2)
+            {
+                ans.push_back(val1);
             i++;
-        }
-         while(j>=0){
-            ans.push_back(nums[j]*nums[j]);
+            }
+            else
+            {
+                ans.push_back(val2);
             j--;
+            }
+                
         }
+        
         return ans;
     }
 };
