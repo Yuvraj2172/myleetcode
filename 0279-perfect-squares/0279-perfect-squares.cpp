c681ad1547109d52ab6b070e230cccebb4451c0d
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-04-13 12:31:26 +0530

    Time: 109 ms (85.11%), Space: 9.2 MB (57.35%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n+1);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<n+1;i++){
+            int mini = INT_MAX;
+            for(int j=1;j<=sqrt(i);j++){
+                int rem = i- j*j;
+                if(dp[rem] < mini){
+                    mini = dp[rem];
+                }
+            }
+            dp[i] = mini +1;
+        }
+        return dp[n];
+    }
+};
