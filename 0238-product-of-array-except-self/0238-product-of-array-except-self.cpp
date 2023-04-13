332596059ff05b014aa0ce92558f92d01ec72b48
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-04-13 13:00:33 +0530

    Time: 26 ms (64.51%), Space: 25.2 MB (24.01%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans(n);
-        vector<int> leftproduct(n);
-        vector<int> rightproduct(n);
-        int prod =1;
-        leftproduct[0]=1;
+         int n = nums.size();
+        vector<int> lp(n), rp(n), ans(n);
+        int p =1;
+        lp[0]=1;
         for(int i=1;i<n;i++){
-            leftproduct[i] = leftproduct[i-1]* nums[i-1];
-            prod = leftproduct[i];
+            lp[i]=p*nums[i-1];
+            p = lp[i];
         }
-        
-        prod =1;
-        rightproduct[n-1]=1;
-        for(int i=n-2;i>=0;i--){
-            rightproduct[i] = prod*nums[i+1];
-            prod = rightproduct[i];
+        // for(auto it : lp)cout<<it<<" ";
+        // cout<<"\n";
+        p=1;
+        rp[n-1]=1;
+        for(int i = n-2;i>=0;i--){
+            rp[i] = p*nums[i+1];
+            p= rp[i];
         }
+        // for(auto it : rp)cout<<it<<" ";
+        // cout<<"\n";
         for(int i=0;i<n;i++){
-            ans[i] = leftproduct[i] * rightproduct[i];
+            ans[i] = lp[i]*rp[i];
         }
         return ans;
     }
