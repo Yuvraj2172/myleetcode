8997b5e33cc6b3d1c1c8273ae63805d6cf8f8261
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-04-15 15:26:46 +0530

    Time: 81 ms (81.58%), Space: 14.5 MB (75.76%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -3,12 +3,12 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         vector<int> dp(amount+1, amount+1);
         dp[0]=0;
-        for(int i = 1;i<amount+1;i++){
-            for(int j=0; j<coins.size();j++){
-                if( i - coins[j]>=0 )dp[i] = min(dp[i], 1+ dp[i - coins[j]]);
+        for(auto coin : coins){
+            for(int i = coin ;i <= amount;i++){
+                dp[i] = min(dp[i] , dp[i- coin]+1);
             }
         }
-    if(dp[amount]!=amount+1)return dp[amount];
-    return -1;
+        if(dp[amount] <= amount)return dp[amount];
+        return -1;
     }
 };
