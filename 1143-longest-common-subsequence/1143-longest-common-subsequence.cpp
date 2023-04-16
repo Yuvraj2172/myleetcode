f00447eff01746e4dfaffab6b4c49428fd6d753d
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-04-16 21:01:16 +0530

    Time: 24 ms (93.55%), Space: 10.7 MB (74.11%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string s, string t) {
-        int m =s.size();
-        int n = t.size();
-        int **output = new int*[m+1];
-        for(int i=0;i<=m;i++){
-        output[i] = new int[n+1];    
-        }
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.size() , n = text2.size();
+        int dp[m+1][n+1];
+        memset(dp, 0 , sizeof(dp));
         
-        for(int j=0;j<=n;j++){
-output[0][j] = 0;
-        }
-        for(int i =0;i<=m;i++){
-output[i][0] = 0;
-        }
-        for(int i=1;i<=m;i++){
-            for(int j = 1;j<=n;j++){
-            if(s[m-i]==t[n-j]){
-                output[i][j] = 1 + output[i-1][j-1];    
-            }
+        for(int  i =m-1;i>=0;i--){
+            for(int j = n-1; j>=0;j--){
+                if(text1[i] == text2[j]){
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                }
                 else {
-                    int a = output[i-1][j];
-                    int b = output[i][j-1];
-                    int c = output[i-1][j-1];
-                    output[i][j] = max(a, max(b,c));
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
                 }
             }
         }
-        return output[m][n];
+        return dp[0][0];
     }
 };
