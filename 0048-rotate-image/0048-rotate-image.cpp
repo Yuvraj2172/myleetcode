fb30188a96ac1704f845a57f831bd27e5eaa1d6c
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-04-26 08:56:18 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (43.25%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,22 +2,13 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        int m = matrix[0].size();
-        vector<vector<int>> v(n , vector<int> (m , -1));
-         int col = n-1;
-        for(int i=0;i<n ; i++){
-            int row = 0;
-            for(int j =0; j<m;j++){
-                v[row][col]=matrix[i][j];
-                row++;
+        for(int i=0 ;i<n;i++){
+            for(int j =0; j<i;j++){
+                swap(matrix[i][j], matrix[j][i]);
             }
-            col--;
         }
         for(int i=0;i<n;i++){
-            for(int j =0; j < m; j++){
-                matrix[i][j] = v[i][j];
-            }
-            // cout<<"\n";
+                reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
