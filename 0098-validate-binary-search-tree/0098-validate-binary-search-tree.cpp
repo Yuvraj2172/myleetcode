b3fe682316b7515ea3bc2806c252bc783a826b3b
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-05-19 14:31:06 +0530

    Time: 26 ms (5.15%), Space: 21.7 MB (78.91%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,11 +1,12 @@
+ 
 class Solution {
 public:
-    bool helper(TreeNode* root , long min , long max){
-        if(root == NULL)return true;
-        if(root->val >= max || root->val <= min)return false;
-        return helper(root->left , min , root->val) && helper(root->right, root->val, max);
+    bool helper(TreeNode* root, long min , long max){
+        if(root== NULL)return true;
+        if(root->val >= max  || root->val <= min)return false;
+        return helper(root->left, min , root->val) && helper(root->right , root->val , max);
     }
     bool isValidBST(TreeNode* root) {
-        return helper(root , LONG_MIN, LONG_MAX);
+        return helper(root , LONG_MIN , LONG_MAX);
     }
 };
