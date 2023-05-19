2a6c08e5f09b95275a5a18f876d3e2de05e9cae9
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-05-19 14:21:01 +0530

    Time: 41 ms (29.32%), Space: 26 MB (78.36%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    TreeNode* helper(vector<int>& preorder, int prestart , int preend, vector<int>& inorder, int instart , int inend){
-        if(instart> inend || prestart > preend)return NULL;
+    TreeNode* helper(vector<int>& preorder , int prestart, int preend , vector<int>& inorder, int instart ,int inend){
+        if(instart > inend || prestart > preend)return NULL;
+        int k =-1;
         int rootdata = preorder[prestart];
         TreeNode* root = new TreeNode(rootdata);
-        int k =-1;
-        for(int i=instart;i<=inend; i++){
+        for(int i = instart ; i <=inend; i++){
             if(inorder[i] == rootdata){
-                k = i;
+                k=i;
                 break;
             }
         }
-        root->left = helper(preorder, prestart +1 ,prestart + (k - instart) , inorder, instart, k-1);
-        root->right= helper(preorder, prestart + ( k - instart)+1, preend , inorder, k+1, inend);
-        return root;
-    }
+        root->left = helper(preorder , prestart +1 , prestart + k - instart , inorder , instart , k-1);
+        root->right = helper(preorder , prestart + k - instart +1, preend , inorder , k+1, inend);
+                             return root;
+   }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int prestart =0;
-        int preend  = preorder.size()-1;
-        int instart =0;
-        int inend=inorder.size()-1;
-        return helper(preorder, prestart , preend, inorder, instart, inend);
+        int prestart =0, preend = preorder.size()-1 , instart =0 , inend = inorder.size()-1;
+        return helper(preorder , prestart , preend , inorder ,instart , inend);
     }
 };
