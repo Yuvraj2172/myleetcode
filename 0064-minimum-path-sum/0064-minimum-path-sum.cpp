711655d7d8386dc916498f33c55de1e370c9dfbc
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-06-01 12:22:03 +0530

    Time: 11 ms (60.65%), Space: 10.2 MB (21.74%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int helper(int m , int n , vector<vector<int>>& grid, vector<vector<int>>&dp){
-        if(m==0 && n==0)return grid[m][n];
+        if(m==0 && n==0)return dp[0][0];
         if(m<0 || n<0)return 201;
         if(dp[m][n]!=-1)return dp[m][n];
         int up = grid[m][n]+helper(m-1,n, grid,dp);
@@ -13,6 +13,7 @@ public:
         int m= grid.size()-1;
         int n =  grid[0].size()-1;
         vector<vector<int>> dp(m+1, vector<int>(n+1,-1));
+        dp[0][0] = grid[0][0];
         return helper(m , n , grid, dp);
     }
 };
