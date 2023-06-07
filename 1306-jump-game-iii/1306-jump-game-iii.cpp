fe2edc4684232a279f3bb05cb1db057e9ca27343
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-06-07 10:32:55 +0530

    Time: 53 ms (45.58%), Space: 30.8 MB (98.09%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    bool helper(vector<int>& arr , int start ,int len,vector<int>&vis){
-        if(start<0 || start >= len)return false;
-        if(vis[start]==2)return false;
-        if(arr[start] == 0)return true;
-        vis[start]+=1;
-        bool forward = helper(arr, start + arr[start], len,vis);
-        bool backward = helper(arr, start - arr[start], len,vis);
-        return forward || backward;
+    bool helper(vector<int>&arr, int start , int len){
+        if(start<0 || start>= len)return false;
+        if(arr[start]<0)return false;
+        if(arr[start]==0)return true;
+        arr[start] = -arr[start];
+        return helper(arr, start + arr[start], len) or helper(arr, start - arr[start], len);
     }
     bool canReach(vector<int>& arr, int start) {
-        // cout<<arr[0] +12;
         int len = arr.size();
-        vector<int> vis(len , 0);
-        return helper(arr, start, len,vis);
-        // return 1;
+        return helper(arr , start,  len);
     }
 };
