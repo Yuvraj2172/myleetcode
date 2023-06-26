81cb15e963c87629c111c52106305d1a051959a2
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-06-26 12:52:44 +0530

    Time: 120 ms (54.68%), Space: 49.8 MB (10.51%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1, amount+1);
-        dp[0]=0;
-        for(auto coin : coins){
-            for(int i = coin ;i <= amount;i++){
-                dp[i] = min(dp[i] , dp[i- coin]+1);
+    int helper(int ind , vector<int>& coins , int target, vector<vector<int>>&dp){
+        if(ind==0){
+            if(target%coins[ind] ==0){
+                return target / coins[ind];
             }
+            else return 1e9;
         }
-        return dp[amount]<= amount ? dp[amount]: -1;
+        if(dp[ind][target]!=-1)return dp[ind][target];
+        int nontake = helper(ind-1 , coins , target,dp);
+        int take = 1e9;
+        if(coins[ind]<= target)take = 1 + helper(ind , coins , target - coins[ind],dp);
+        return dp[ind][target]=min(take , nontake);
+    }
+    int coinChange(vector<int>& coins, int target) {
+        int  n = coins.size();
+        vector<vector<int>> dp(n+1, vector<int>(target+1, -1));
+        int ans =  helper(n-1, coins, target , dp);
+        if(ans == 1e9)return -1;
+        return ans;
     }
 };
