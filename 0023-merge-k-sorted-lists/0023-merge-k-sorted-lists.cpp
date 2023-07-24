94631fd24a3e3faaaf06d0225878545b70fc1acc
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-07-24 15:24:01 +0530

    Time: 27 ms (64.49%), Space: 13.9 MB (17.18%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,35 +1,24 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        vector<int> v;
+        priority_queue<int, vector<int> , greater<int>> pq;
         int n = lists.size();
-        for(int i=0;i<n ;i++){
+        for(int i=0;i<n;i++){
             ListNode* temp = lists[i];
-            while(temp!=nullptr){
-                v.push_back(temp->val);
+            while(temp!=NULL){
+                pq.push(temp->val);
                 temp = temp->next;
             }
-            
         }
-        // for(auto it : v)cout<< it<<" ";
-        sort(v.begin(), v.end());
-        ListNode* res = new ListNode(0);
-        ListNode* temp = res;
-        for(auto it : v){
-            ListNode* newNode = new ListNode(it);
-            temp->next = newNode;
-            temp = newNode;
+        ListNode* ans = new ListNode(0);
+        ListNode* temp = ans;
+        while(!pq.empty()){
+            int t = pq.top();
+            pq.pop();
+            ListNode* newNode = new ListNode(t);
+            temp->next = newNode ;
+            temp = temp->next;
         }
-        return res->next;
+        return ans->next;
     }
 };
