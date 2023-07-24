a1b0a666338983be6cdc511c728e60bdc8b404cd
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-07-24 15:18:51 +0530

    Time: 5 ms (63.11%), Space: 7.2 MB (31.84%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    bool dp(int i ,int j , string& s , string& p , vector<vector<int>>& memo){
-        if(memo[i][j] !=-1)return memo[i][j];
+    bool helper(int i , int j , string& s, string& p, vector<vector<int>>& dp){
+        if(dp[i][j]!=-1)return dp[i][j];
         if(j== p.length()){
-            memo[i][j] = (i==s.length()) ? 1:0;
-            return memo[i][j];
+            dp[i][j] = (i==s.length())? 1:0;
+            return dp[i][j];
         }
-        bool firstmatch = (i<s.length() && (s[i] == p[j] || p[j] == '.'));
-        if(j+1< p.length() && p[j+1] == '*'){
-            memo[i][j] = (dp(i,j+2, s,p,memo) || (firstmatch && dp(i+1, j , s,p, memo))) ? 1:0;
+        bool firstmatch = (i<s.length() && (s[i]==p[j] || p[j]=='.'));
+        if(j+1< p.length() && p[j+1]=='*'){
+            dp[i][j] = ( helper(i, j+2, s,p,dp) || firstmatch && helper(i+1, j , s,p, dp)) ? 1:0;
         }
         else{
-            memo[i][j] = (firstmatch && dp(i+1, j+1 , s,p,memo)) ? 1:0;
+            dp[i][j] = firstmatch && helper(i+1, j+1, s,p, dp);
         }
-        return memo[i][j];
+        return dp[i][j];
     }
     bool isMatch(string s, string p) {
-        int m = s.length();
-        int n = p.length();
-        vector<vector<int>> memo(m+1, vector<int>(n+1,-1));
-        return dp(0,0, s,p,memo);
+        int n = s.length() , m = p.length();
+        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+        return helper(0,0, s, p, dp);
     }
 };
