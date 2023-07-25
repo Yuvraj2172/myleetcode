1405dbfcd510fbf7a036d2eb2e0286b6885154fe
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-07-25 18:59:57 +0530

    Time: 27 ms (9.56%), Space: 20.5 MB (29.54%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -6,15 +6,15 @@ public:
         vector<int> suffix(n);
         prefix[0] = height[0];
         suffix[n-1] = height[n-1];
-        for(int i=1;i<n;i++){
-            prefix[i] = max(height[i], prefix[i-1]);
+        for(int i=1; i<n;i++){
+            prefix[i] = max(prefix[i-1] , height[i]);
         }
         for(int i=n-2;i>=0;i--){
-            suffix[i] = max(height[i], suffix[i+1]);
+            suffix[i] = max(suffix[i+1], height[i]);
         }
         int ans =0;
         for(int i=0;i<n;i++){
-            ans += min(prefix[i], suffix[i]) - height[i];
+            ans+= min(prefix[i], suffix[i]) -  height[i];
         }
         return ans;
     }
