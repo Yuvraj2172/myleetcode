c135e6675430aa57a8308eb2a2d6f1217611f8fb
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-09-06 23:59:54 +0530

    Time: 6 ms (63.09%), Space: 9.1 MB (5.29%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
@@ -2,26 +2,29 @@ class Solution {
 public:
     vector<ListNode*> splitListToParts(ListNode* head, int parts) {
         int n =0;
-        ListNode* t2 = head;
-        while(t2){
-            t2 = t2->next;
-            n++; 
+        ListNode* temp = head;
+        while(temp){
+            n++;
+            temp = temp->next;
         }
-        int width = n / parts, rem = n%parts;
+        int width = n / parts;
+        int rem = n % parts;
         vector<ListNode*> ans;
         ListNode* cur = head;
-        for(int i=0; i< parts; i++){
-            ListNode* ithpart = new ListNode(0);
-            ListNode* temp = ithpart;
-            // int add = (rem !=  0) ? 1 : 0;
-            for(int j = 0; j< width + (i< rem ? 1 : 0) ; j++){
-                ListNode* newNode = new ListNode(cur->val);
-                temp -> next = newNode;
+        for(int i=0; i<parts; i++){
+            ListNode* ithPart = new ListNode(0);
+            ListNode* temp = ithPart;
+            int limit = width;
+            if(rem!=0){
+                limit = width +1;
+                rem--;
+            }
+            for(int j=0; j < limit; j++){
+                temp->next = new ListNode(cur->val);
                 temp = temp->next;
                 if(cur)cur = cur->next;
             }
-            // rem--;
-            ans.push_back(ithpart->next);
+            ans.push_back(ithPart->next);
         }
         return ans;
     }
