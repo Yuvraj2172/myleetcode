7481a5010394e723c7e6b38a5254ec9e0e12002e
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-09-09 00:00:58 +0530

    Time: 80 ms (78.38%), Space: 67.8 MB (40.66%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,11 +3,13 @@ public:
     int maxSubArray(vector<int>& nums) {
         int maxi = INT_MIN;
         int sum=0;
-        for(auto it : nums){
-            sum += it;
-            maxi=max(maxi , sum);
-            if(sum<0)sum=0;
+        for(auto it:  nums){
+            sum+=it;
+            maxi = max(maxi , sum);
+            if(sum < 0)sum=0;
         }
         return maxi;
     }
 };
+
+   
