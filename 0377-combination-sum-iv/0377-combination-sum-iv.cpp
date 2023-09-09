29231db86d17b02ff226ece3784951011b74899f
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-09-09 17:26:43 +0530

    Time: 3 ms (50.37%), Space: 6.8 MB (12.44%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        int tn = nums.size();
+        int n = nums.size();
         sort(nums.begin(), nums.end());
-        vector<unsigned long long > dp(target+1,0);
+        vector<unsigned long long> dp(target+1,0);
         dp[0]=1;
         for(int i=1;i<=target;i++){
             for(int x : nums){
