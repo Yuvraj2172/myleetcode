d501cb29b6d2f5df4248535c46db39299a738c34
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-09-12 00:15:34 +0530

    Time: 68 ms (80.23%), Space: 27 MB (57.56%) - LeetHub

--- 0915-generate-random-point-in-a-circle/0915-generate-random-point-in-a-circle.cpp
+++ 0915-generate-random-point-in-a-circle/0915-generate-random-point-in-a-circle.cpp
@@ -1,23 +1,18 @@
 class Solution {
-    double R,X,Y;
+    double r, xc, yc;
+
 public:
-    Solution(double radius, double x_center, double y_center) : R(radius), X(x_center), Y(y_center) {
-        srand(time(0));
+    Solution(double r, double xc, double yc) {
+        this->r = r; this->xc = xc; this->yc = yc;
     }
     double random(){
-        return (double)rand() / RAND_MAX;
+        return (double) rand() / RAND_MAX;
     }
     vector<double> randPoint() {
-        vector<double> ans(2);
-        double r = sqrt(random()) * R , pi = 2*M_PI * random();
-        ans[0] = X + r*cos(pi);
-        ans[1] = Y + r*sin(pi);
-        return ans;
+        double angle = random()*2*M_PI;
+        double constR = sqrt(random()) * r;
+        return {xc + constR*cos(angle), yc + constR*sin(angle)};
+
+
     }
 };
-
-/**
- * Your Solution object will be instantiated and called as such:
- * Solution* obj = new Solution(radius, x_center, y_center);
- * vector<double> param_1 = obj->randPoint();
- */
