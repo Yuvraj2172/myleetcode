b9df4e3b31c37d853be5df4e225c3f1e0ac1a7a5
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-09-12 13:04:35 +0530

    Added solution - LeetHub

--- Postorder Traversal - GFG/postorder-traversal.cpp
+++ Postorder Traversal - GFG/postorder-traversal.cpp
@@ -111,15 +111,22 @@ int main()
 }
 // } Driver Code Ends
 
-void helper(Node* root, vector<int>& ans){
-    if(!root)return;
-    if(root->left)helper(root->left,ans);
-    if(root->right)helper(root->right, ans);
-    ans.push_back(root->data);
-}
 vector <int> postOrder(Node* root)
-{
-vector<int> ans;
-    helper(root, ans);
+{   
+    if(!root)return {};
+    vector<int> ans;
+    stack<Node*> st1, st2;
+    st1.push(root);
+    while(!st1.empty()){
+        Node* node = st1.top();
+        st1.pop();
+        st2.push(node);
+        if(node->left)st1.push(node->left);
+        if(node->right)st1.push(node->right);
+    }
+    while(!st2.empty()){
+        ans.push_back(st2.top()->data);
+        st2.pop();
+    }
     return ans;
 }
