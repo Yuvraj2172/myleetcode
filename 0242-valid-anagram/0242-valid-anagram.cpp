716774d22f832a3bbac7d6128b50c191ae3f69ab
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-09-21 00:21:56 +0530

    Time: 11 ms (48.43%), Space: 7.6 MB (9.58%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,7 +4,8 @@ public:
         if(s.size()!=t.size())return 0;
         sort(s.begin(),s.end());
         sort(t.begin(), t.end());
-        for(int i=0; i<s.size();i++)if(s[i]!=t[i])return 0;
-        return true;
+        if(s==t)return 1;
+
+        return false;
     }
 };
