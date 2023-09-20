4034c74703382a4575185e3030939292d939fdbb
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-09-20 23:54:26 +0530

    Time: 8 ms (36.31%), Space: 7.5 MB (8.72%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.size()!= t.size())return 0;
+        unordered_map<char,char> mp1;
+        unordered_map<char,char> mp2;
+        for(int i=0; i<s.size();i++){
+          if(mp1.find(s[i])==mp1.end()){
+            mp1[s[i]] = t[i];
+          }
+          else if(mp1[s[i]]!=t[i]){
+            return false;
+          }
+          if(mp2.find(t[i]) == mp2.end()){
+            mp2[t[i]] = s[i];
+          }
+          else if(mp2[t[i]]!= s[i]){
+            return false;
+          }
+        }
+        return true;
+    }
+};
