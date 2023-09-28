1746e1e6059d977e93620d851cb5fc324b959773
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-09-28 10:17:58 +0530

    Time: 7 ms (66.47%), Space: 16.7 MB (12.92%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        vector<int> e,o;
-        for(auto it : nums){
-            if(it&1)o.push_back(it);
-            else e.push_back(it);
-        }
         vector<int> ans;
-        for(auto it : e)ans.push_back(it);
-        for(auto it : o)ans.push_back(it);
+        for(auto it : nums){
+            if(it%2==0)ans.push_back(it);
+        }
+        for(auto it : nums){
+            if(it&1)ans.push_back(it);
+        }
         return ans;
     }
 };
