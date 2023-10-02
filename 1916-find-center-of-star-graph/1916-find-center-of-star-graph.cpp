1103dd9abb9f4629bb3c1faee4b817c58a28953c
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-10-02 14:12:12 +0530

    Time: 152 ms (73.26%), Space: 67.8 MB (50.64%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
@@ -1,20 +1,3 @@
-
-int init = [] {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    ofstream out("user.out");
-    cout.rdbuf(out.rdbuf());
-    for (string line; getline(cin, line); cout << '\n') {
-        istringstream ss(line);
-        char ch;
-        int a, b, c, d;
-        ss >> ch >> ch >> a >> ch >> b >> ch >> ch >> ch >> c >> ch >> d;
-        if (a == c || a == d) cout << a;
-        else cout << b;
-    }
-    exit(0);
-    return 0;
-}();
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
