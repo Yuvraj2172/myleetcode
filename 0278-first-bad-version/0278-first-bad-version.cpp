e6b83419510f300c458135043838e666574dccc6
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-10-04 16:29:54 +0530

    Time: 2 ms (56.69%), Space: 6.2 MB (20.88%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int start =0;
-        int end = n;
-        int res=0;
+        int start =0, end = n;
+        int ans = 0;
         while(start <= end){
             int mid = start + (end - start)/2;
-            bool ans  = isBadVersion(mid);
-            if(ans){
-                res = mid;
-                end = mid-1; continue;
+            bool f = isBadVersion(mid);
+            if(f){
+                ans = mid;
+                end = mid - 1; continue;
             }
-            start = mid+1;
+            start = mid + 1;
         }
-        return res;
+        return ans;
     }
 };
