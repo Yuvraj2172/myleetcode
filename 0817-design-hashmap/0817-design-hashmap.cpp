55bf4c987385682d0e70e3afb51a088181889cbd
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-10-04 11:00:21 +0530

    Time: 110 ms (60.96%), Space: 52.7 MB (77.92%) - LeetHub

---
+++ 0817-design-hashmap/0817-design-hashmap.cpp
@@ -0,0 +1,21 @@
+class MyHashMap {
+public:
+    unordered_map<int,int> mp;
+    MyHashMap() {
+        
+    }
+    
+    void put(int key, int value) {
+        mp[key] = value;
+    }
+    
+    int get(int key) {
+        if(mp.find(key)!= mp.end())
+        return mp[key];
+        return -1;
+    }
+    
+    void remove(int key) {
+        mp.erase(key);
+    }
+};
