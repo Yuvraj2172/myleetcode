b5f0e52103c2ddfc43fe0b6609b786dfd3b776cf
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-10-10 02:30:10 +0530

    Time: 2 ms (87.93%), Space: 6.9 MB (71.72%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+         string ans = "1";
+         int i(1);
+         while(i<n){
+             string curr = "";
+             for(int j(0); j< ans.size(); j++){
+                 int count(1);
+                 while(j+1 < ans.size() && ans[j] == ans[j+1]){
+                     count +=1;
+                     j++;
+                 }
+             curr += to_string(count) + ans[j];
+             }
+             i++;
+             ans = curr;
+         }
+         return ans;
+    }
+};
