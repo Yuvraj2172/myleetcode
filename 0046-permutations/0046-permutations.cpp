117b13ca3da2a6ab0d60b2b097c0beb06c184bb4
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2023-10-10 01:41:58 +0530

    Time: 8 ms (5.76%), Space: 9.1 MB (5.65%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    void helper(int ind, vector<int>& temp, vector<int>& nums, vector<vector<int>>& ans, map<int,int>& freq){
-        if(ind == nums.size()){
-            ans.push_back(temp);
+    void helper(int index, vector<int> temp, vector<int>& nums, set<vector<int>>& s){
+        if(index == nums.size()){
+            s.insert(temp);
             return;
         }
-        for(int i=0;i<nums.size();i++){
-            if(freq.find(nums[i]) == freq.end()){
-                freq[nums[i]]=1;
-                temp.push_back(nums[i]);
-                helper(ind+1 , temp , nums, ans , freq);
-                freq.erase(nums[i]);
-                temp.pop_back();
-            }
+        for(int i= index; i< temp.size(); i++){
+            swap(temp[i], temp[index]);
+            helper(index +1, temp, nums ,s);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> temp;
-        map<int,int> freq;
-        helper(0 , temp , nums, ans , freq);
-        return ans;
+        vector<vector<int>> op;
+        vector<int> temp = nums;
+        set<vector<int>> s;
+        helper(0 , temp , nums, s);
+        for(auto it : s)op.push_back(it);
+        return op;
     }
 };
