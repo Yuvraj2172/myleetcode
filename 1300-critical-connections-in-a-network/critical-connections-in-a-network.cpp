d1f037dba856279f19ecabd803daf1d3ec6291f0
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2024-01-22 20:13:34 +0530

    Time: 415 ms (91.20%) | Memory: 174.9 MB (55.03%) - LeetSync

--- 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
@@ -1,40 +1,40 @@
 class Solution {
 private:
     int timer = 1;
-    void dfs(int node, int parent, vector<int> &vis,
-    vector<int> adj[], int tin[], int low[], vector<vector<int>> &bridges) {
-        vis[node] = 1;
-        tin[node] = low[node] = timer;
-        timer++;
-        for (auto it : adj[node]) {
-            if (it == parent) continue;
-            if (vis[it] == 0) {
-                dfs(it, node, vis, adj, tin, low, bridges);
-                low[node] = min(low[it], low[node]);
-                
-                if (low[it] > tin[node]) {
-                    bridges.push_back({it, node});
+   void helper(int node, int parent, vector<int>& vis, vector<int> adj[], int tin[],
+        int low[], vector<vector<int>>& bridges){
+            vis[node] = 1;
+            tin[node] = low[node] = timer;
+            timer++;
+            for(auto it : adj[node]){
+                if(it == parent)continue;
+                if(vis[it] == 0){
+                    helper(it, node, vis, adj, tin, low, bridges);
+                    low[node] = min(low[it], low[node]);
+                    if(low[it] > tin[node]){
+                        bridges.push_back({it, node});
+                    }
+                    else {
+                        low[node] = min(low[node], low[it]);
+                    }
                 }
-            }
-            else {
-                low[node] = min(low[node], low[it]);
+                else {
+                        low[node] = min(low[node], low[it]);
+                    }
             }
         }
-    }
 public:
     vector<vector<int>> criticalConnections(int n,
     vector<vector<int>>& connections) {
         vector<int> adj[n];
-        for (auto it : connections) {
-            int u = it[0], v = it[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+        for(auto it : connections){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }
-        vector<int> vis(n, 0);
-        int tin[n];
-        int low[n];
         vector<vector<int>> bridges;
-        dfs(0, -1, vis, adj, tin, low, bridges);
+        vector<int> vis(n,0);
+        int tin[n], low[n];
+        helper(0,-1, vis, adj, tin, low, bridges);
         return bridges;
     }
 };
