e23072f05fac8eb75546c86279fa716c464ef0be
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2024-01-22 22:21:42 +0530

    Time: 371 ms (15.72%) | Memory: 131.6 MB (8.76%) - LeetSync

--- 164-maximum-gap/maximum-gap.cpp
+++ 164-maximum-gap/maximum-gap.cpp
@@ -1,10 +1,49 @@
+
+
+// int maximumGap(vector<int> &nums)
+// {
+//     int n = nums.size();
+//     if (n < 2)
+//         return 0;
+//     vector<int> sortednums = countsort(nums);
+
+//     int maxgap = INT_MIN;
+//     for (int i = 1; i < n; i++)
+//     {
+//         int currgap = sortednums[i] - sortednums[i - 1];
+//         maxgap = max(maxgap, currgap);
+//     }
+
+//     return maxgap;
+// }
+
 class Solution {
 public:
+
+   vector<int> helper(vector<int> &nums)
+{
+    int n = nums.size();
+    map<int, int> m;
+    for (int i = 0; i < n; i++ )
+        m[nums[i]]++;
+
+    nums.clear();
+    for (auto it : m)
+    {
+        int cnt = it.second;
+        while (cnt--)
+        {
+            nums.push_back(it.first);
+        }
+    }
+    return nums;
+}
     int maximumGap(vector<int>& nums) {
-        if(nums.size()==1)return 0;
-        sort(begin(nums), end(nums));
-        int diff = nums[1] - nums[0];
+        if(nums.size()<2)return 0;
         int n = nums.size();
+        
+        vector<int> nigga = helper(nums);
+        int diff = INT_MIN;
         for(int i=1; i<n;i++){
             diff = max(diff, nums[i] - nums[i-1]);
         }
