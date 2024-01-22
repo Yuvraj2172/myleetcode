2185a063da37fe548c01f0ebd00201a3f09e3260
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2024-01-22 21:41:42 +0530

    Time: 4 ms (85.39%) | Memory: 9.7 MB (5.12%) - LeetSync

---
+++ 903-implement-rand10-using-rand7/implement-rand10-using-rand7.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rand10() {
+        while(1){
+            int row = 7*(rand7()-1);
+            int col = rand7();
+            int num = row + col;
+            if(num <= 40){
+                return (num)%10+ 1 ;
+            }
+        }
+        return 0;
+    }
+};
