419b673efab033aa41949e5e1cba8ffe86016734
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2024-01-22 22:25:36 +0530

    Time: 393 ms (11.49%) | Memory: 131.6 MB (8.72%) - LeetSync

--- 164-maximum-gap/maximum-gap.cpp
+++ 164-maximum-gap/maximum-gap.cpp
@@ -1,52 +1,24 @@
-
-
-// int maximumGap(vector<int> &nums)
-// {
-//     int n = nums.size();
-//     if (n < 2)
-//         return 0;
-//     vector<int> sortednums = countsort(nums);
-
-//     int maxgap = INT_MIN;
-//     for (int i = 1; i < n; i++)
-//     {
-//         int currgap = sortednums[i] - sortednums[i - 1];
-//         maxgap = max(maxgap, currgap);
-//     }
-
-//     return maxgap;
-// }
-
 class Solution {
 public:
-
-   vector<int> helper(vector<int> &nums)
-{
-    int n = nums.size();
-    map<int, int> m;
-    for (int i = 0; i < n; i++ )
-        m[nums[i]]++;
-
-    nums.clear();
-    for (auto it : m)
-    {
-        int cnt = it.second;
-        while (cnt--)
-        {
-            nums.push_back(it.first);
+    vector<int> helper(vector<int>& nums, int n){
+        map<int,int> mp;
+        for(auto it : nums)mp[it]++;
+        nums.clear();
+        for(auto it : mp){
+            while(it.second--){
+                nums.push_back(it.first);
+            }
         }
+        return nums;
     }
-    return nums;
-}
     int maximumGap(vector<int>& nums) {
-        if(nums.size()<2)return 0;
         int n = nums.size();
-        
-        vector<int> nigga = helper(nums);
-        int diff = INT_MIN;
+        if(n<2)return 0;
+        vector<int> arr = helper(nums,n);
+        int ans = INT_MIN;
         for(int i=1; i<n;i++){
-            diff = max(diff, nums[i] - nums[i-1]);
+            ans = max(ans, arr[i] - arr[i-1]);
         }
-        return diff;
+        return ans;
     }
 };
