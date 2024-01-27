b83383165dd568d0486580f213ca774224531915
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2024-01-28 02:10:36 +0530

    Time: 30 ms (92.60%) | Memory: 33.9 MB (13.34%) - LeetSync

--- 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.cpp
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.cpp
@@ -1,40 +1,14 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
         if(head == NULL || head->next == NULL)return head;
-        vector<int> v;
-        ListNode* temp = head;
-        while(temp){
-            v.push_back(temp->val);
-            temp = temp->next;
+        ListNode* curr = head;
+        while(curr->next){
+            ListNode* d = new ListNode(__gcd(curr->val, curr->next->val));
+            d->next = curr->next;
+            curr->next = d;
+            curr = d->next;
         }
-        
-        vector<int> t;
-        for(int i=0; i<v.size()-1; i++){
-            int curr = v[i];
-            int next = v[i+1];
-            int g= __gcd(curr, next);
-            t.push_back(curr);
-            t.push_back(g);
-            // t.push_back(next);
-        }
-        t.push_back(v[v.size()-1]);
-        ListNode* ans = new ListNode(0);
-        temp = ans;
-        for(auto it : t){
-            temp -> next = new ListNode(it);
-            temp = temp->next;
-        }
-        return ans->next;
+        return head;
     }
 };
