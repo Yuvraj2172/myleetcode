6f0c6ceeb1b0bc73799fc589bf9201ff6ed4944b
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2024-09-26 22:49:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -10,19 +10,9 @@ public:
             int istart = it.first;
             int iend = it.second;
 
-            if(istart < end && iend > start){
-                return 0;
-            }
-
-            
+            if(iend > start)return false;
         }
         v.push_back(make_pair(start,end));
         return 1;
     }
 };
-
-/**
- * Your MyCalendar object will be instantiated and called as such:
- * MyCalendar* obj = new MyCalendar();
- * bool param_1 = obj->book(start,end);
- */
