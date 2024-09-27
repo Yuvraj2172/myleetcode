def9391de3b206269a057a121c2ce1b2a8cf05fe
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2024-09-27 15:55:03 +0530

    Time: 1822 ms (5%), Space: 108.7 MB (5.63%) - LeetHub

---
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -0,0 +1,31 @@
+class MyCalendarTwo {
+public:
+    vector<pair<int,int>> booking;
+    vector<pair<int,int>> doublebooking;
+    MyCalendarTwo() {
+        
+    }
+    
+    bool book(int start, int end) {
+        for(auto e : doublebooking){
+            if(max(e.first, start) < min(e.second, end)){
+                return 0;
+            }
+        }
+        for(auto e : booking){
+            if(max(e.first, start), min(e.second, end)){
+                doublebooking.push_back({max(e.first, start), min(e.second, end)});
+            }
+        }
+
+        booking.push_back({start,end});
+        return 1;
+
+    }
+};
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+ */
