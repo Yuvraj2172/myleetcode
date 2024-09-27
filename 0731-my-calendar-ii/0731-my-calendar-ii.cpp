df8f7df442ba5f9a93bb0a7c21b8a8046e6d3c54
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2024-09-27 17:07:43 +0530

    Time: 64 ms (95.72%), Space: 37.8 MB (99.06%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.cpp
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -1,27 +1,29 @@
 class MyCalendarTwo {
 public:
-    vector<pair<int,int>> booking;
-    vector<pair<int,int>> doublebooking;
+    vector<pair<int,int>> booking, doublebooking;
     MyCalendarTwo() {
         
     }
     
     bool book(int start, int end) {
-        for(auto e : doublebooking){
-            if(max(e.first, start) < min(e.second, end)){
-                return 0;
-            }
-        }
-        for(auto e : booking){
-            if(max(e.first, start), min(e.second, end)){
-                doublebooking.push_back({max(e.first, start), min(e.second, end)});
-            }
+        for(auto it : doublebooking){
+            if(max(it.first, start) < min(it.second, end))return false;
         }
 
+        for(auto it: booking){
+            if(max(it.first, start) < min(it.second, end)){
+                doublebooking.push_back(
+                    {max(it.first, start), min(it.second, end)}
+                );
+            }
+        }
         booking.push_back({start,end});
         return 1;
-
     }
 };
 
-auto io_opt = [] { ios::sync_with_stdio(false); cin.tie(nullptr); return 0; }();
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+ */
