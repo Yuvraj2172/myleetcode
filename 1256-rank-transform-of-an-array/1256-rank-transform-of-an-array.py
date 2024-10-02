ebab2bfa82a9425a1bcc8d15348c89e6e15d91e0
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2024-10-02 19:51:04 +0530

    Time: 250 ms (37.59%), Space: 35 MB (60.73%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
@@ -1,11 +1,12 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        mp = {}
+        
         unique_list = sorted(list(set(arr)))
+        mp = {}
 
         for i in range(len(unique_list)):
-            mp[unique_list[i]] = i + 1
-        
+            mp[unique_list[i]] = i+1
+
         for i in range(len(arr)):
             arr[i] = mp[arr[i]]
 
