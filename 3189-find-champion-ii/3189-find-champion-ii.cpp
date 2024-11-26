94dabab6900edf358a19cd015453280ea73d2f53
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2024-11-26 14:28:47 +0530

    Time: 0 ms (100%), Space: 93.3 MB (75.38%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.cpp
+++ 3189-find-champion-ii/3189-find-champion-ii.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int findChampion(int n, vector<vector<int>>& edges) {
-       
-        vector<int> adj(n,0);
-        for(int i=0; i<edges.size();i++){
-            // int u = edges[i][0];
-            int v = edges[i][1];
-           adj[v] = 1;
-        
-        }
-        vector<int> ans;
+        bitset<100> bs;
 
-        for(int i =0; i<n;i++){
-            if(adj[i] == 0){
-                ans.push_back(i);
+        for(const auto&  it : edges){
+            bs.set(it[1]);
+        }
+        int ans = -1;
+
+        for(int i=0; i<n; i++){
+            if(!bs[i]){
+                if(ans != -1){
+                    return -1;
+                }
+
+                ans = i;
             }
         }
-
-        if(ans.size() > 1)return -1;
-        return ans[0];
+        return ans;
     }
 };
