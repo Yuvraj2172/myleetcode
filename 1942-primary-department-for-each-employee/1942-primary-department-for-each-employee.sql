73e2c61f4faa61b47cc3db9a55f0672e28beb68f
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2024-11-29 17:00:11 +0530

    Time: 539 ms (81.67%), Space: 0B (100%) - LeetHub

--- 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -1,9 +1,8 @@
 SELECT employee_id, department_id
-from EMPLOYEE
-WHERE primary_flag = 'Y'
-or employee_id in (
-    SELECT employee_id
-    from employee
-    group by employee_id
-    having count(department_id) = 1
-)
+FROM Employee
+WHERE primary_flag='Y' OR 
+    employee_id in
+    (SELECT employee_id
+    FROM Employee
+    Group by employee_id
+    having count(employee_id)=1)
