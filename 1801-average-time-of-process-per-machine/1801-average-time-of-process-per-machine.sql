56a43ea9e3dc4029c84a535cda1d078c54f98231
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2024-11-30 19:34:54 +0530

    Time: 240 ms (61.44%), Space: 0B (100%) - LeetHub

--- 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -7,9 +7,7 @@ on a1.machine_id = a2.machine_id
 and 
 a1.process_id = a2.process_id
 and
-a1.activity_type <> a2.activity_type
-and 
-a1.timestamp > a2.timestamp
+a1.activity_type = 'end' and a2.activity_type = 'start'
 group by 
 a1.machine_id
 
