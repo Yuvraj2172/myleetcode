708546d48969fbc487741051458b2b9b119b66b8
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2024-11-30 21:56:57 +0530

    Time: 555 ms (59.41%), Space: 0B (100%) - LeetHub

--- 1523-capital-gainloss/1523-capital-gainloss.sql
+++ 1523-capital-gainloss/1523-capital-gainloss.sql
@@ -1,10 +1,3 @@
--- select a.*,
--- b.*
--- from stocks a left join 
--- stocks b 
--- using(stock_name)
--- and a.operation = 'Buy' and b.operation.
-
 select stock_name,
 sum(case 
     when operation = 'Buy' then -price
