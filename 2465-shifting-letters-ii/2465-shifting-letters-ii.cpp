7e24a01d56ae14e44a3fa5585b69a68698526f23
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2025-01-05 21:38:28 +0530

    Time: 0 ms (100%), Space: 99.2 MB (43.08%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+    string shiftingLetters(string s, vector<vector<int>>& arr) {
         int n = s.size();
-    vector<int> diff(n + 1, 0);
-
-    // Apply the shifts to the difference array
-    for (const auto& shift : shifts) {
-        int start = shift[0], end = shift[1], direction = shift[2];
-        int shiftValue = (direction == 1) ? 1 : -1;
-        diff[start] += shiftValue;
-        if (end + 1 < n) {
-            diff[end + 1] -= shiftValue;
+        vector<int> diff(n + 1, 0);
+        for(int i=0; i < arr.size(); i++){
+            int start = arr[i][0], end = arr[i][1];
+            int dir = (arr[i][2] == 1) ? 1 : -1;
+            diff[start] += dir;
+            if(end + 1 < n){
+                diff[end + 1] -= dir;
+            }
         }
-    }
-
-    // Compute the net shifts using prefix sum
-    vector<int> netShifts(n, 0);
-    int currentShift = 0;
-    for (int i = 0; i < n; ++i) {
-        currentShift += diff[i];
-        netShifts[i] = currentShift;
-    }
-
-    // Apply the net shifts to the string
-    for (int i = 0; i < n; ++i) {
-        int originalIndex = s[i] - 'a';
-        int newIndex = (originalIndex + netShifts[i]) % 26;
-        if (newIndex < 0) {
-            newIndex += 26; // Handle negative shifts
+        vector<int> pref(n,0);
+        int curr = 0;
+        for(int i=0; i<n;i++){
+            curr += diff[i];
+            pref[i] += curr;
         }
-        s[i] = 'a' + newIndex;
-    }
 
-    return s;
+        for(int i=0; i<n;i++){
+            int orig = s[i] - 'a';
+            int newind = (orig + pref[i]) % 26;
+            if(newind < 0){
+                newind += 26;
+            }
+            s[i] = 'a' + newind;
+        }
+        return s;
     }
 };
