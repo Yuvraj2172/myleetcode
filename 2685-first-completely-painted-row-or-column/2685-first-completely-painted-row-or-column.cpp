98cd10f3b92524a90dd574b3ef3036e37b8a7d1d
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2025-01-21 02:53:31 +0530

    Time: 90 ms (74.1%), Space: 171 MB (28.01%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
-         int n = mat.size();
-         int m = mat[0].size();
-         vector<int> rc(n,m),cc(m,n);
-
-         unordered_map<int, pair<int,int>> mp;
-         for(int i = 0; i < n; i++){
-            for(int j = 0; j < m ; j++){
+        int n = mat.size(), m = mat[0].size();
+        vector<int> rc(n,m), cc(m,n);
+        unordered_map<int, pair<int,int>> mp;
+        for(int i=0; i<n; i++){
+            for(int j =0; j < m; j++){
                 mp[mat[i][j]] = {i,j};
             }
-         }
-
-         for(int i =0; i< arr.size(); i++){
+        }
+        for(int i=0; i < arr.size(); i++){
             int val = arr[i];
             auto [r,c] = mp[arr[i]];
 
-            if(--rc[r] == 0 || --cc[c] == 0)return i;
-         }
-         return 0;
+            if(--rc[r] == 0 || --cc[c]== 0 )return i;
+        }
+        return 0;
     }
 };
