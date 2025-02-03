e6d3deff3bbddcaa4e1885592ed924f52ff961f9
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2025-02-03 14:18:08 +0530

    Time: 1117 ms (31.2%), Space: 0B (100%) - LeetHub

---
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -0,0 +1,3 @@
+select cte.request_at as 'Day',round(sum(cte.counter/cte.ttp),2) as 'Cancellation Rate' from(select 
+(case when status = 'completed' then 0
+else 1 end) as counter, request_at, count(status) over(partition by request_at) as ttp from Trips where client_id in (select users_id from Users where banned = 'No') and driver_id in (select users_id from Users where banned = 'No')) as cte group by cte.request_at having cte.request_at between '2013-10-01' and '2013-10-03'
