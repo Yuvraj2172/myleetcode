2f0fd08f15dbe93bb2607010779ebe31795bee5a
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2025-02-03 15:36:22 +0530

    Time: 1362 ms (14.43%), Space: 0B (100%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -1,3 +1,19 @@
-select cte.request_at as 'Day',round(sum(cte.counter/cte.ttp),2) as 'Cancellation Rate' from(select 
-(case when status = 'completed' then 0
-else 1 end) as counter, request_at, count(status) over(partition by request_at) as ttp from Trips where client_id in (select users_id from Users where banned = 'No') and driver_id in (select users_id from Users where banned = 'No')) as cte group by cte.request_at having cte.request_at between '2013-10-01' and '2013-10-03'
+with cte as (
+    select t.request_at,
+    t.status
+    from trips t 
+    join users u1 on t.client_id = u1.users_id
+    join users u2 on t.driver_id = u2.users_id
+    where t.request_at between"2013-10-01" and "2013-10-03"
+    and u1.banned = 'No' and u2.banned = 'No' 
+
+)
+
+select request_at as Day, 
+ROUND(SUM(Case 
+        when status != "completed" then 1 
+        else 0
+    End    )/ COUNT(*),2) as "Cancellation Rate" from cte
+group by request_at
+
+
