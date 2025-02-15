12603c43624f186fa0b2779bec64547c0086ab51
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2025-02-15 22:43:08 +0530

    Time: 2 ms (89.67%), Space: 7.9 MB (91.69%) - LeetHub

---
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int helper(int sq, int n){
+        if(sq == n)return 1;
+        if(sq < n || n <0)return 0;
+        return (helper(sq / 10, n - sq % 10) || 
+                helper(sq / 100, n - sq % 100) || 
+                helper(sq / 1000, n - sq % 1000) );
+    }
+    int punishmentNumber(int n) {
+        int ans = 0;
+        for(int i=0; i <= n; i++){
+            int sq = i * i;
+            if(helper(sq, i))ans+=sq;
+        }
+        return ans;
+    }
+};
