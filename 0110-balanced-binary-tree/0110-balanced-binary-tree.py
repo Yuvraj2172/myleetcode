b0b614434fe2f26477d438157d5710d034d84d71
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2025-05-18 01:00:02 +0530

    Time: 3 ms (67.77%), Space: 18.9 MB (63.64%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -1,18 +1,11 @@
-
 class Solution:
-    ans = True
-    def helper(self, root):
-        if self.ans == False:
-            return 0 
-        if root is None:
-            return 0
-        left_check = self.helper(root.left)
-        right_check = self.helper(root.right)
-
-        if abs(left_check - right_check) > 1:
-            self.ans = False
-        return 1 + max(left_check, right_check)
-        
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        self.helper(root)
-        return self.ans
+        def do_dfs(root):
+            if not root:
+                return [True,0]
+            left, right = do_dfs(root.left), do_dfs(root.right)
+
+            balanced = left[0] and right[0] and abs(left[1] - right[1])<=1 
+
+            return [balanced, 1 + max(left[1], right[1])]
+        return do_dfs(root)[0]
