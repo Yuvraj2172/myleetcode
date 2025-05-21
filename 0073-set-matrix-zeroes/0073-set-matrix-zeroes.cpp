b0a121bb32c0200737e2a5e88ce08803df18dbb1
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2025-05-21 15:14:22 +0530

    Time: 0 ms (100%), Space: 18.7 MB (24.6%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& nums) {
-        vector<pair<int, int>> v;
-        int m = nums.size();
-        int n = nums[0].size();
-        for(int i=0; i<m;i++){
-            for(int j = 0; j < n;j++){
-                if(nums[i][j]==0){
-                    v.push_back({i ,j});
+    void setZeroes(vector<vector<int>>& arr) {
+        vector<int> r,c;
+        int m = arr.size();
+        int n = arr[0].size();
+        for(int i=0; i < m; i++){
+            for(int j=0; j < n; j++){
+                if(arr[i][j] == 0){
+                    r.push_back(i);
+                    c.push_back(j);
                 }
             }
         }
-        // for(auto it : v)cout<<it.first<<" "<<it.second<<"\n";
-        for(auto it : v){
-            for(int i= 0 ; i<m;i++){
-                nums[i][it.second] =0;
+        for(auto ri : r){
+            for(int j=0; j < n; j++){
+                arr[ri][j] =0;
             }
-            for(int j = 0; j<n;j++){
-                nums[it.first][j] = 0;
+        }
+        for(auto ci : c){
+            for(int i=0; i < m; i++){
+                arr[i][ci] =0;
             }
         }
     }
