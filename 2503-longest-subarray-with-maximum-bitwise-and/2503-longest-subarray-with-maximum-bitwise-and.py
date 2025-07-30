da78ccc15997ea9258d0ec96290c7e0b89058f59
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2025-07-30 23:29:39 +0530

    Time: 23 ms (88.17%), Space: 31.4 MB (8.28%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.py
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.py
@@ -2,4 +2,4 @@ from itertools import groupby
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         maxi = max(nums)
-        return max(sum(1 for item in group )for key, group in groupby(nums) if key == maxi)
+        return max(len(list(group))for key, group in groupby(nums) if key == maxi)
