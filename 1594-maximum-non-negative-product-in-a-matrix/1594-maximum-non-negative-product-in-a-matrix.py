7bf327e98e2eb66f54910e1c0787bf1515eb24f3
Yuvraj Soni <77541888+Yuvraj2172@users.noreply.github.com>
2026-03-23 10:33:42 +0530

    Time: 1 ms (92.11%), Space: 19.3 MB (88.6%) - LeetHub

--- 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.py
+++ 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.py
@@ -1,36 +1,32 @@
 class Solution:
     def maxProductPath(self, grid: List[List[int]]) -> int:
-        if not grid or not grid[0]:
-            return 0
-        
-        rows, cols = len(grid), len(grid[0])
-        MOD = 10**9 + 7
-        dp = [[[0, 0] for _ in range(cols)] for _ in range(rows)]
-        dp[0][0] = [grid[0][0], grid[0][0]]
-        
-        # Fill first column
-        for i in range(1, rows):
-            val = grid[i][0]
-            dp[i][0][0] = dp[i-1][0][0] * val
-            dp[i][0][1] = dp[i-1][0][1] * val
-        
-        # Fill first row
+        row, cols, MOD = len(grid), len(grid[0]), 10 ** 9 + 7
+
+        max_dp = [[0] * cols for _ in range(row)]
+        min_dp = [[0] * cols for _ in range(row)]
+
+        max_dp[0][0] = min_dp[0][0] = grid[0][0]
+
         for j in range(1, cols):
-            val = grid[0][j]
-            dp[0][j][0] = dp[0][j-1][0] * val
-            dp[0][j][1] = dp[0][j-1][1] * val
-        
-        # Fill rest
-        for i in range(1, rows):
+            max_dp[0][j] = max_dp[0][j-1] * grid[0][j]
+            min_dp[0][j] = min_dp[0][j-1] * grid[0][j]
+        for i in range(1, row):
+            max_dp[i][0] = max_dp[i-1][0] * grid[i][0]
+            min_dp[i][0] = min_dp[i-1][0] * grid[i][0]
+
+        for i in range(1, row):
             for j in range(1, cols):
                 val = grid[i][j]
-                
-                if val >= 0:
-                    dp[i][j][0] = min(dp[i-1][j][0], dp[i][j-1][0]) * val
-                    dp[i][j][1] = max(dp[i-1][j][1], dp[i][j-1][1]) * val
-                else:
-                    dp[i][j][0] = max(dp[i-1][j][1], dp[i][j-1][1]) * val
-                    dp[i][j][1] = min(dp[i-1][j][0], dp[i][j-1][0]) * val
+
+                possible_values = [
+                    max_dp[i][j-1] * val,
+                    max_dp[i-1][j] * val,
+                    min_dp[i][j-1] * val,
+                    min_dp[i-1][j] * val
+                ]
+
+                max_dp[i][j] = max(possible_values)
+                min_dp[i][j] = min(possible_values)
         
-        max_prod = dp[-1][-1][1]
-        return max_prod % MOD if max_prod >= 0 else -1
+        ans = max_dp[-1][-1]
+        return ans % MOD if ans >=0 else -1
